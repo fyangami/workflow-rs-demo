d05f8148d3c4a28f1b1a6e7606f518651bcf6c46
fyang <fyangami@163.com>
2024-04-09 05:53:52 +0000

    feat: add axum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use axum::{extract::Path, routing::get, Router};
 
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/:name", get(handler));
 
-fn main() {
-    println!("hello world!")
+    // run it
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:80").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler(Path(name): Path<String>) -> String {
+    format!("Hey👋, {}\n", name)
 }
